d9700646422cef4f283de616af3fc5a1174ae89f
Ylendiaz <ylendiazpolanco@gmail.com>
2021-05-22 05:35:37 -0400

    Fixed Print line by line all anagrams

--- Anagrams.py
+++ Anagrams.py
@@ -4,6 +4,7 @@ from collections import defaultdict
 from itertools import islice
 
 counter = 0
+count = 1
 with open("words.txt") as file:
     lines = (x[:-1].lower() for x in file.readlines())
     words = list(islice(lines, 3000))
@@ -17,12 +18,18 @@ for w in words:
 
 anagrams = [tuple(r) for v in d.values() for r in combinations(v, 2)]
 
-
 print ("\nLas palabras anagramas en el documento son:\n")
 
-for words in anagrams:
-    for i in words:
-        print(list(dict.fromkeys(anagrams[counter])))
-        counter += 1 
+for line in anagrams:
+    for i in line:
+        final = (list(dict.fromkeys(anagrams[counter])))
+        if len(final)>1:
+            print(final)
+        counter += 1
+        
+
+        
+        
+        
 
 print("")      
