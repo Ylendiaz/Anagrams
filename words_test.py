a7a26d555bd7c3c50500dc563e1635278d0c462c
Ylendiaz <ylendiazpolanco@gmail.com>
2021-05-22 07:40:03 -0400

    FINAL

--- words_test.py
+++ words_test.py
@@ -1,11 +1,24 @@
 import Anagrams
 import unittest
 
-class Test_AreAnagrams(unittest.TestCase):
-    
-    def test_Anagrams(self):
-        f = ('aam', 'ama')
-        self.assertTrue(f == Anagrams.anagrams[0])
+class Test_GetAnagrams(unittest.TestCase):
+
+    def test_getAnagrams(self):
+        Anagrams.wordList = ['amor', 'huevo', 'roma']
+        self.assertTrue(Anagrams.getAnagram(Anagrams.wordList) == [('amor', 'roma')])
+
+
+class Test_NotDuplicate(unittest.TestCase):
+
+    def test_isNotDuplicated(self):
+        line = ('amor', 'roma')
+        self.assertTrue(Anagrams.isNotDuplicated(line))
+
+class Test_Duplicate(unittest.TestCase):
+
+    def test_isDuplicated(self):
+        line = ('amor', 'amor')
+        self.assertFalse(Anagrams.isNotDuplicated(line))
 
     
 if __name__ == '__main__':
