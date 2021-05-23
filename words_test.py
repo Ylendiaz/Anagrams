ca7b741f535406b199d2e66eb567533c302cfe92
Ylendiaz <ylendiazpolanco@gmail.com>
2021-05-23 02:22:26 -0400

    Menor tiempo de ejecución.Final

--- words_test.py
+++ words_test.py
@@ -1,24 +1,24 @@
-import Anagrams
+import AnagramsFromFile
 import unittest
 
-class Test_GetAnagrams(unittest.TestCase):
+class Test_GetAnagramsFromFile(unittest.TestCase):
 
-    def test_getAnagrams(self):
-        Anagrams.wordList = ['amor', 'huevo', 'roma']
-        self.assertTrue(Anagrams.getAnagram(Anagrams.wordList) == [('amor', 'roma')])
+    def test_getAnagramsFromFile(self):
+        AnagramsFromFile.wordList = ['amor', 'huevo', 'roma']
+        self.assertTrue(AnagramsFromFile.getAnagram(AnagramsFromFile.wordList) == [('amor', 'roma')])
 
 
 class Test_NotDuplicate(unittest.TestCase):
 
     def test_isNotDuplicated(self):
         line = ('amor', 'roma')
-        self.assertTrue(Anagrams.isNotDuplicated(line))
+        self.assertTrue(AnagramsFromFile.isNotDuplicated(line))
 
 class Test_Duplicate(unittest.TestCase):
 
     def test_isDuplicated(self):
         line = ('amor', 'amor')
-        self.assertFalse(Anagrams.isNotDuplicated(line))
+        self.assertFalse(AnagramsFromFile.isNotDuplicated(line))
 
     
 if __name__ == '__main__':
